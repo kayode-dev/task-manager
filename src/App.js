10cf2296daed3b7538a517cbf7d1648e85bb2db2
Kayode <babalolakayode44@gmail.com>
2023-09-12 11:01:31 +0100

    delete functionality and tasklist UI changes

--- src/App.js
+++ src/App.js
@@ -39,17 +39,29 @@ function App() {
   useEffect(() => {
     fetchTask();
   }, []);
+  const deleteTask = async (key) => {
+    const res = await fetch(
+      `https://swmovies-default-rtdb.firebaseio.com/task/${key}.json`,
+      {
+        method: "delete",
+        ContentType: "application/json",
+      }
+    );
+    fetchTask();
+  };
   return (
     <div className="min-h-full flex flex-col items-center justify-evenly gap-20">
       <h1 className="text-white font-bold text-2xl mt-10 text-center">
-        Numerous Cheffing - The Todo list
+        Numerous Cheffings - The Todo list
       </h1>
-      <div className="bg-white p-8 h-min lg:w-2/5 md:w-3/5 w-[90%] flex items-center justify-center rounded">
+      <div className="bg-white p-8 h-min lg:w-1/2 md:w-3/5 w-[90%] flex items-center justify-center  rounded">
         <AddTask onAddTask={addTask} />
       </div>
-      <div className="bg-white p-8 h-min lg:w-2/5 md:w-3/5 w-[90%] max-h-[55vh] overflow-y-scroll rounded">
+      <div className="bg-white p-8 h-min lg:w-1/2 md:w-3/5 w-[90%] max-h-[50vh] overflow-y-auto rounded">
         {isLoading && <p>Loading....</p>}
-        {!isLoading && tasks.length > 0 && <TaskList Tasks={tasks} />}
+        {!isLoading && tasks.length > 0 && (
+          <TaskList Tasks={tasks} onDelete={deleteTask} />
+        )}
         {!isLoading && tasks.length === 0 && (
           <p>You haven't added any task yet</p>
         )}
--- src/components/tasklist.jsx
+++ src/components/tasklist.jsx
@@ -1,14 +1,24 @@
 const TaskList = (props) => {
+  const deleteHandler = (e) => {
+    const keytoDelete = e.target.getAttribute("data-value").toString();
+    console.log(keytoDelete);
+    props.onDelete(keytoDelete);
+  };
   return (
-    <ul className="flex flex-col gap-3">
+    <ul className="flex flex-col gap-3 ">
       {props.Tasks.map((task) => {
         return (
-          <li className="flex flex-col gap-3">
-            <div className="flex gap-4">
-              <input type="checkbox"/>
-              <p key={task.id} className="md:min-w-full">
-                {task.task}
-              </p>{" "}
+          <li className="flex flex-col gap-3" key={task.id}>
+            <div className="flex gap-4 justify-between items-center">
+              <input type="checkbox" />
+              <p className="md:w-4/5">{task.task}</p>{" "}
+              <button
+                className="bg-red-700 p-2 rounded text-white"
+                onClick={deleteHandler}
+                data-value={task.id}
+              >
+                Delete
+              </button>
             </div>
             <hr />
           </li>
